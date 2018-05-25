30d9e5aa2ddf58687b1fcd78ab6d4716f97b413e
Derrick Liu <dliu@lediur.com>
2018-05-24 23:37:25 -0700

    Add GdiGraphics stub for on_paint

--- src/types/gdi.d.ts
+++ src/types/gdi.d.ts
@@ -136,3 +136,104 @@ declare namespace gdi {
   //   Returns a unique id.
   //   */
 }
+
+interface GdiGraphics {
+  /*
+  Typically used inside on_paint(gr)
+  There are many different ways to get colours.
+  Use window.GetColourDUI/window.GetColourCUI,
+  RGB function from helpers.txt, utils.ColourPicker,
+  etc.
+  */
+  // CalcTextHeight(str, IGdiFont); (uint)
+  // // This will only calulate the text height of one line.
+  // CalcTextWidth(str, IGdiFont); (uint)
+  // DrawEllipse(x, y, w, h, line_width, colour); (void)
+  // DrawImage(IGdiBitmap, dstX, dstY, dstW, dstH, srcX, srcY, srcW, srcH[, angle][, alpha]); (void)
+  // /*
+  // angle: default 0
+  // alpha: default 255. Valid values 0-255.
+  // */
+  // DrawLine(x1, y1, x2, y2, line_width, colour); (void)
+  // DrawPolygon(colour, line_width, points); (void)
+  // // points: An array.
+  // DrawString(str, IGdiFont, colour, x, y, w, h[, flags]); (void)
+  // // flags: default 0. See flags.txt > StringFormatFlags
+  // DrawRect(x, y, w, h, line_width, colour); (void)
+  // DrawRoundRect(x, y, w, h, arc_width, arc_height, line_width, colour); (void)
+  // EstimateLineWrap(str, IGdiFont, max_width); (VBArray)
+  // /*
+  // returns a VBArray so you need to use .toArray() on the result.
+  // index | meaning
+  // [0] text line 1
+  // [1] width of text line 1 (in pixel)
+  // [2] text line 2
+  // [3] width of text line 2 (in pixel)
+  // ...
+  // [2n + 2] text line n
+  // [2n + 3] width of text line n (px)
+  // */
+  // FillEllipse(x, y, w, h, colour); (void)
+  // FillGradRect(x, y, w, h, angle, colour1, colour2[, focus]); (void)
+  // /*
+  // focus: default 1. Valid values between 0 and 1.
+  // Specify where the centred colour will be at its highest intensity.
+  // NOTE: This may appear buggy depending on rectangle size. The easiest fix is
+  // to adjust the "angle" by a degree or two.
+  // */
+  // FillPolygon(colour, fillmode, points); (void)
+  // // fillmode: 0 alternate, 1 winding.
+  // // points: An array.
+  // FillRoundRect(x, y, w, h, arc_width, arc_height, colour); (void)
+  // FillSolidRect(x, y, w, h, colour); (void)
+  // GdiAlphaBlend(IGdiRawBitmap, dstX, dstY, dstW, dstH, srcX, srcY, srcW, srcH[, alpha]); (void)
+  // // alpha: default 255. Valid values 0-255.
+  // GdiDrawBitmap(IGdiRawBitmap, dstX, dstY, dstW, dstH, srcX, srcY, srcW, srcH); (void)
+  // // Always faster than DrawImage, does not support alpha channel.
+  // GdiDrawText(str, IGdiFont, colour, x, y, w, h[, format]); (VBArray)
+  // /*
+  // format: default 0. See flags.txt > DT_*
+  // Returns a VBArray so you need to use .toArray() on the result.
+  // index | meaning
+  // [0] left   (DT_CALCRECT)
+  // [1] top    (DT_CALCRECT)
+  // [2] right  (DT_CALCRECT)
+  // [3] bottom (DT_CALCRECT)
+  // [4] characters drawn
+  // */
+  // MeasureString(str, IGdiFont, x, y, w, h[, flags]); (IMeasureStringInfo)
+  // // flags: default 0. See flags.txt > StringFormatFlags
+  //   interface IMeasureStringInfo {
+  //     Properties:
+  //       chars; (int) (read)
+  //       Height; (float) (read)
+  //       lines; (int) (read)
+  //       x; (float) (read)
+  //       y; (float) (read)
+  //       Width; (float) (read)
+  //       /*
+  //       Example:
+  //       // ==PREPROCESSOR==
+  //       // @import "%fb2k_component_path%docs\flags.txt"
+  //       // @import "%fb2k_component_path%docs\helpers.txt"
+  //       // ==/PREPROCESSOR==
+  //       var sf = StringFormat(StringAlignment.Near, StringAlignment.Near);
+  //       var text = utils.ReadTextFile("z:\\info.txt");
+  //       var font = window.GetFontDUI(0);
+  //       function on_paint(gr) {
+  //         gr.DrawString(text, font, RGB(255, 0, 0), 0, 0, window.Width, window.Height, sf);
+  //         var temp = gr.MeasureString(text, font, 0, 0, window.Width, 10000, sf);
+  //         // If we want to calculate height, we must set the height to be far larger than what
+  //         // the text could possibly be.
+  //         console.log(temp.Height); // 2761.2421875 // far larger than my panel height!
+  //         console.log(temp.Chars); // 7967
+  //       }
+  //       */
+  //   }
+  // SetInterpolationMode(mode); (void)
+  // // mode: default 0. See flags.txt > InterpolationMode
+  // SetSmoothingMode(mode); (void)
+  // // mode: default 0. See flags.txt > SmoothingMode
+  // SetTextRenderingHint(mode); (void)
+  // // mode: default 0. See flags.txt > TextRenderingHint
+}
